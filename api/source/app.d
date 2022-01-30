da1ec92727126d8f703e19e5bc46eb846405c53e
CodeMyst <code@myst.rs>
2022-01-30 23:13:40 +0100

    services, postgres test
    Signed-off-by: CodeMyst <code@myst.rs>

--- api/source/app.d
+++ api/source/app.d
@@ -1,15 +1,25 @@
 import vibe.d;
-import pastemyst.config;
+import poodinis;
+import pastemyst.controllers.test;
+import pastemyst.services.config;
+import pastemyst.services.postgres;
 
 void main()
 {
-    Config config = loadConfig("config.json");
+    auto configService = new ConfigService("config.json");
+
+    auto dependencies = new shared DependencyContainer();
+    dependencies.register!ConfigService().existingInstance(configService);
+    dependencies.register!PostgresService();
+
+    dependencies.register!TestController();
 
     auto router = new URLRouter();
+    router.registerRestInterface(dependencies.resolve!TestController());
 
     auto serverSettings = new HTTPServerSettings();
     serverSettings.bindAddresses = ["127.0.0.1"];
-    serverSettings.port = config.port;
+    serverSettings.port = configService.config.port;
 
     listenHTTP(serverSettings, router);
 
