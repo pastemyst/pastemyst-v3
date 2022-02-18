6487afcacd45156cd804093609fe03afd371b71a
CodeMyst <code@myst.rs>
2022-02-18 13:29:14 +0100

    [client] started work on the command palette

--- client/src/routes/__layout.svelte
+++ client/src/routes/__layout.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
     import Header from "../components/Header.svelte";
+    import CommandPalette from "../components/CommandPalette.svelte";
+    import { onMount } from "svelte";
+    import { isCommandPaletteOpen } from "../stores";
 
     import "../app.scss";
+
+    let commandPalette: CommandPalette;
+
+    const onKeyUp = async (e: KeyboardEvent) => {
+        if (e.key === "Escape") {
+            isCommandPaletteOpen.update(open => !open);
+        }
+    };
+
+    onMount(() => {
+        document.addEventListener("keyup", onKeyUp);
+    });
 </script>
 
 <div id="container">
@@ -9,3 +24,5 @@
 
     <slot />
 </div>
+
+<CommandPalette bind:this={commandPalette} />
