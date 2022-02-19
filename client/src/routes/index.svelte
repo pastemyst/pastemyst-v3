053344cabb0fc2e371e75a21c68cf4f63096d7e8
CodeMyst <code@myst.rs>
2022-02-19 19:14:07 +0100

    [client] paste title input

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let expiresIn = "never";
+</script>
+
+<div class="title-input flex sm-row">
+    <label class="hidden" for="paste-title">paste title</label>
+    <input type="text" placeholder="title" id="paste-title" name="paste-title" />
+
+    <button>expires in: {expiresIn}</button>
+</div>
+
+<style lang="scss">
+    .title-input {
+        margin-top: 2rem;
+
+        input {
+            width: 100%;
+            border-top-right-radius: 0;
+            border-bottom-right-radius: 0;
+        }
+
+        button {
+            border-top-left-radius: 0;
+            border-bottom-left-radius: 0;
+            white-space: nowrap;
+        }
+
+        @media screen and (max-width: $break-med) {
+            input {
+                border-radius: $border-radius;
+                border-bottom-left-radius: 0;
+                border-bottom-right-radius: 0;
+            }
+
+            button {
+                border-radius: $border-radius;
+                border-top-left-radius: 0;
+                border-top-right-radius: 0;
+                text-align: left;
+            }
+        }
+    }
+</style>
