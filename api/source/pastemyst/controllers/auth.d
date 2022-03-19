d5dca4ea490374bea22c4d872bf8d7933cbf8cf8
CodeMyst <code@myst.rs>
2022-03-19 20:24:51 +0100

    storing OAuth state in the session

--- api/source/pastemyst/controllers/auth.d
+++ api/source/pastemyst/controllers/auth.d
@@ -28,8 +28,39 @@ public class AuthWebController
     }
 
     @path("login/github")
-    public void getGitHubLogin() @safe
+    public void getGitHubLogin(HTTPServerRequest req, HTTPServerResponse res) @safe
     {
-        redirect(authService.getAuthorizationUrl(authService.githubProvider));
+        import std.random : rndGen;
+        import std.algorithm : map, filter;
+        import std.range : take;
+        import std.array : appender;
+        import std.base64 : Base64;
+        import std.ascii : isAlphaNum;
+
+        // generate a random state string for oauth
+        auto rndNums = rndGen().map!(a => cast(ubyte) a)().take(32);
+        auto apndr = appender!string();
+        Base64.encode(rndNums, apndr);
+        auto state = apndr.data.filter!isAlphaNum().to!string();
+
+        auto session = req.session;
+        if (!session) session = res.startSession();
+        session.set("oauth_state", state);
+
+        res.redirect(authService.getAuthorizationUrl(authService.githubProvider, state));
+    }
+
+    @path("/login/github-callback")
+    public void getGithubCallback(HTTPServerRequest req, HTTPServerResponse res, string code, string state) @safe
+    {
+        if (!req.session)
+        {
+            throw new HTTPStatusException(HTTPStatus.badRequest,
+                "OAuth callback called but the session hasn't been started.");
+        }
+
+        string sessionState = req.session.get!string("oauth_state");
+
+        if (state != sessionState) throw new HTTPStatusException(HTTPStatus.badRequest, "Invalid state code.");
     }
 }
