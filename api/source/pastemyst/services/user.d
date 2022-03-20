28255bb7995238f0011c83124d16c001e0339dc3
CodeMyst <code@myst.rs>
2022-03-20 21:52:53 +0100

    inserting new users into the DB

--- api/source/pastemyst/services/user.d
+++ api/source/pastemyst/services/user.d
@@ -1,6 +1,10 @@
 module pastemyst.services.user;
 
 import pastemyst.services;
+import pastemyst.models;
+import pastemyst.encoding;
+
+@safe:
 
 public class UserService
 {
@@ -9,5 +13,30 @@ public class UserService
     public this(MongoService mongoService)
     {
         this.mongoService = mongoService;
+    }
+
+    /**
+     * Checks if a user exists with the provided OAtuh provider id.
+     */
+    public bool existsByProviderId(const string provider, const string id)
+    {
+        return !mongoService.findOne!User(["oauthProviderIds." ~ provider: id]).isNull();
+    }
+
+    /**
+     * Inserts a user into the DB. Modifies the provided user's ID field with the generated one.
+     */
+    public void createUser(ref User user)
+    {
+        // make sure there's no accidental overlap
+        string id;
+        do
+        {
+            id = randomBase36();
+        } while (!mongoService.findOneById!User(id).isNull());
+
+        user.id = id;
+
+        mongoService.insert!User(user);
     }
 }
