ba87f394c9b74f130682f9e2c443e1dbb50a770e
CodeMyst <code@myst.rs>
2022-03-23 22:41:30 +0100

    checking if username taken

--- client/src/routes/create-account.svelte
+++ client/src/routes/create-account.svelte
@@ -3,27 +3,41 @@
     import { getUser } from "../api/user";
 
     let username = $page.url.searchParams.get("username");
+    let usernameInput: HTMLInputElement;
     let usernameTaken = false;
 
-    const onUsernameInput = async () => {
+    const onFormSubmit = async () => {
         usernameTaken = (await getUser(username) !== null);
+
+        if (usernameTaken) usernameInput.focus();
     };
 </script>
 
 <section>
     <h2>create a new account</h2>
 
-    <form class="flex col">
-        <label for="username">username:<span class="required">*</span></label>
-        <input required type="text" name="username" id="username" placeholder="username..." bind:value={username} on:input={onUsernameInput} />
+    <form class="flex col" on:submit|preventDefault={onFormSubmit}>
+        <label for="username">
+            username:<span class="required">*</span>
+
+            {#if usernameTaken}
+                <span class="error">username is taken</span>
+            {/if}
+        </label>
+        <input required type="text" name="username" id="username" placeholder="username..." bind:value={username} bind:this={usernameInput} />
 
         <button type="submit" class="btn-main">create account</button>
     </form>
 </section>
 
-<style langs="scss">
+<style lang="scss">
     label {
         line-height: 2rem;
+
+        .error {
+            color: $color-red;
+            font-weight: normal;
+        }
     }
 
     input,
@@ -33,5 +47,12 @@
 
     button {
         margin-top: 2rem;
+    }
+
+    @media screen and (max-width: $break-med) {
+        input,
+        button {
+            width: 100%;
+        }
     }
 </style>
