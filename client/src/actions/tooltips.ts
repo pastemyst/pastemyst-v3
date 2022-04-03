06aa8886b34f5a2044b64e255bf9a1f71e226d58
CodeMyst <code@myst.rs>
2022-04-03 17:32:26 +0200

    [client] tooltips using tippy.js
    Closes #5

---
+++ client/src/actions/tooltips.ts
@@ -0,0 +1,16 @@
+import tippy, { roundArrow } from "tippy.js";
+
+export const tooltip = (node: HTMLElement, params = {}) => {
+    const title = node.title;
+    const label = node.getAttribute("aria-label");
+    const content = title || label;
+
+    const tip = tippy(node, { content, theme: "myst", arrow: roundArrow, ...params });
+
+    return {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        update: (newParams: any) => tip.setProps({ content, ...newParams }),
+
+        destroy: () => tip.destroy()
+    };
+};
--- client/src/components/paste/PasteOptions.svelte
+++ client/src/components/paste/PasteOptions.svelte
@@ -1,22 +1,26 @@
+<script lang="ts">
+    import { tooltip } from "../../actions/tooltips";
+</script>
+
 <div class="paste-options block flex sm-row center space-between">
     <div class="options flex row center">
         <input type="checkbox" id="encrypt" />
-        <label for="encrypt" aria-label="encrypt the paste with a password" class="btn btn-square btn-plain">
+        <label for="encrypt" aria-label="encrypt the paste with a password" class="btn btn-square btn-plain" use:tooltip>
             <ion-icon name="key" />
         </label>
 
         <input type="checkbox" id="private" />
-        <label for="private" aria-label="private paste, only visible by you" class="btn btn-square btn-plain">
+        <label for="private" aria-label="private paste, only visible by you" class="btn btn-square btn-plain" use:tooltip>
             <ion-icon name="lock-closed" />
         </label>
 
         <input type="checkbox" id="show-on-profile" />
-        <label for="show-on-profile" aria-label="show on your public profile" class="btn btn-square btn-plain">
+        <label for="show-on-profile" aria-label="show on your public profile" class="btn btn-square btn-plain" use:tooltip>
             <ion-icon name="people" />
         </label>
 
         <input type="checkbox" id="detach" />
-        <label for="detach" aria-label="detach paste, won't be associated with your account" class="btn btn-square btn-plain">
+        <label for="detach" aria-label="detach paste, won't be associated with your account" class="btn btn-square btn-plain" use:tooltip>
             <ion-icon name="finger-print" />
         </label>
     </div>
