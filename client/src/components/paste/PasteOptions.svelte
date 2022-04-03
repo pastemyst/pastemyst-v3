827362812e0cfd95705b9865ad3eb6134fb3e53a
CodeMyst <code@myst.rs>
2022-04-03 16:36:51 +0200

    [client] more look and feel changes

--- client/src/components/paste/PasteOptions.svelte
+++ client/src/components/paste/PasteOptions.svelte
@@ -1,22 +1,22 @@
 <div class="paste-options block flex sm-row center space-between">
     <div class="options flex row center">
         <input type="checkbox" id="encrypt" />
-        <label for="encrypt" aria-label="encrypt the paste with a password" class="btn btn-square">
+        <label for="encrypt" aria-label="encrypt the paste with a password" class="btn btn-square btn-plain">
             <ion-icon name="key" />
         </label>
 
         <input type="checkbox" id="private" />
-        <label for="private" aria-label="private paste, only visible by you" class="btn btn-square">
+        <label for="private" aria-label="private paste, only visible by you" class="btn btn-square btn-plain">
             <ion-icon name="lock-closed" />
         </label>
 
         <input type="checkbox" id="show-on-profile" />
-        <label for="show-on-profile" aria-label="show on your public profile" class="btn btn-square">
+        <label for="show-on-profile" aria-label="show on your public profile" class="btn btn-square btn-plain">
             <ion-icon name="people" />
         </label>
 
         <input type="checkbox" id="detach" />
-        <label for="detach" aria-label="detach paste, won't be associated with your account" class="btn btn-square">
+        <label for="detach" aria-label="detach paste, won't be associated with your account" class="btn btn-square btn-plain">
             <ion-icon name="finger-print" />
         </label>
     </div>
@@ -41,17 +41,20 @@
         opacity: 0;
 
         &:checked + label {
-            border-color: $color-prim;
+            color: $color-prim;
         }
     }
 
     label {
         margin-right: 1rem;
         font-size: $fs-large;
-        background-color: $color-bg;
+
+        &:hover {
+            background-color: $color-bg;
+        }
     }
 
     button {
-        padding: 0.25rem 0.5rem;
+        padding: 0.5rem 1rem;
     }
 </style>
