0805bf25b2d77e5557921325c9e472d951e821de
CodeMyst <code@myst.rs>
2022-05-07 22:38:08 +0200

    [api] endpoint for getting count of active pastes

--- api/source/pastemyst/controllers/meta_controller.d
+++ api/source/pastemyst/controllers/meta_controller.d
@@ -1,5 +1,6 @@
 module pastemyst.controllers.meta_controller;
 
+import pastemyst.services;
 import vibe.d;
 
 /**
@@ -16,6 +17,13 @@ public interface IMetaController
      * Returns the current version of the website. Based on git tags.
      */
     Json getVersion() @safe;
+
+    /**
+     * GET /api/v3/meta/active_pastes
+     *
+     * Returns the number of currently active (existing) pastes.
+     */
+    Json getActivePastes() @safe;
 }
 
 /**
@@ -25,15 +33,28 @@ public interface IMetaController
  */
 public class MetaController : IMetaController
 {
+    private PasteService pasteService;
+
+    ///
+    public this(PasteService pasteService)
+    {
+        this.pasteService = pasteService;
+    }
+
     public override Json getVersion() @safe
     {
         import std.process : executeShell;
         import std.string : strip;
 
-        // TODO: maybe cache this? not sure if caching this is gonna even improve the performance
+        // TODO: (#41) maybe cache this? not sure if caching this is gonna even improve the performance
         // currently this runs in less than 6ms on my machine
         const res = executeShell("git describe --tags --abbrev=0");
 
         return Json(["version": Json(res.output.strip())]);
+    }
+
+    public override Json getActivePastes() @safe
+    {
+        return Json(["count": Json(pasteService.getNumActivePastes())]);
     }
 }
