baa0aa344207ee8a5b6a64aa75b7778c40fbc80e
CodeMyst <code@myst.rs>
2022-07-11 14:41:04 +0200

    go web server

--- api/main.go
+++ api/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-    "fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-    fmt.Println("hello world")
+	e := echo.New()
+
+	e.GET("/ping", func(ctx echo.Context) error {
+		return ctx.String(http.StatusOK, "pong")
+	})
+
+	e.Logger.Fatal(e.Start(":5000"))
 }
