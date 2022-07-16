cb25f0d8945f7e639ea2bdb5dcc1644a82b929a8
CodeMyst <code@myst.rs>
2022-07-16 15:26:11 +0200

    [client] specifying expiresIn when creating pastes

--- client/src/lib/api/paste.ts
+++ client/src/lib/api/paste.ts
@@ -1,9 +1,21 @@
 import { apiBase } from "./api";
 
+export enum ExpiresIn {
+    never = "never",
+    oneHour = "1h",
+    twoHours = "2h",
+    tenHours = "10h",
+    oneDay = "1d",
+    twoDays = "2d",
+    oneWeek = "1w",
+    oneYear = "1y"
+}
+
 export interface Paste {
     id: string;
     title: string;
     createdAt: Date;
+    expiresIn: ExpiresIn;
     pasties: Pasty[];
 }
 
@@ -15,6 +27,7 @@ export interface Pasty {
 
 export interface PasteSkeleton {
     title: string;
+    expiresIn: ExpiresIn;
     pasties: PastySkeleton[];
 }
 
@@ -22,6 +35,24 @@ export interface PastySkeleton {
     title: string;
     content: string;
 }
+
+/**
+ * Converts a long string to `ExpiresIn` enum.
+ */
+export const expiresInFromString = (s: string): ExpiresIn => {
+    switch (s) {
+        case "never": return ExpiresIn.never;
+        case "1 hour": return ExpiresIn.oneHour;
+        case "2 hours": return ExpiresIn.twoHours;
+        case "10 hours": return ExpiresIn.tenHours;
+        case "1 day": return ExpiresIn.oneDay;
+        case "2 days": return ExpiresIn.twoDays;
+        case "1 week": return ExpiresIn.oneWeek;
+        case "1 year": return ExpiresIn.oneYear;
+    }
+
+    return ExpiresIn.never;
+};
 
 export const createPaste = async (skeleton: PasteSkeleton): Promise<Paste> => {
     const res = await fetch(`${apiBase}/paste/`, {
