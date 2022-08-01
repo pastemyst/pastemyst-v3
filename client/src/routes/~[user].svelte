49238e95aff003427ae788259a85743df6a13eae
CodeMyst <code@myst.rs>
2022-08-01 23:21:59 +0200

    [client] profile layout

--- client/src/routes/~[user].svelte
+++ client/src/routes/~[user].svelte
@@ -16,7 +16,7 @@
 
         let user: User;
         let relativeJoined: string;
-        let isLoggedIn = false;
+        let isCurrentUser = false;
         if (userRes.ok) {
             user = await userRes.json();
             relativeJoined = moment(user.createdAt).fromNow();
@@ -24,7 +24,7 @@
             if (meRes.ok) {
                 const loggedInUser: User = await meRes.json();
 
-                isLoggedIn = loggedInUser.id === user.id;
+                isCurrentUser = loggedInUser.id === user.id;
             }
         }
 
@@ -32,7 +32,7 @@
             status: userRes.status,
             props: {
                 user: user,
-                isLoggedIn: isLoggedIn,
+                isCurrentUser: isCurrentUser,
                 relativeJoined: relativeJoined
             }
         };
@@ -42,84 +42,106 @@
 <script lang="ts">
     export let user: User;
     export let relativeJoined: string;
-    export let isLoggedIn: boolean;
+    export let isCurrentUser: boolean;
 </script>
 
-<section class="user-header flex row center">
-    <img class="avatar" src={user.avatarUrl} alt="${user.username}'s avatar" />
+<div class="layout flex sm-row">
+    <section class="user-header flex sm-col center">
+        <img class="avatar" src={user.avatarUrl} alt="${user.username}'s avatar" />
 
-    <div class="username flex col">
-        <div class="flex row center username-top">
-            <h2>{user.username}</h2>
+        <div class="username flex col">
+            <div class="flex row center username-top">
+                <h2>{user.username}</h2>
 
-            <div class="badges flex row center">
-                {#if user.contributor}
-                    <div use:tooltip aria-label="contributor" class="flex">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="icon contributor"
-                            viewBox="0 0 512 512"
+                <div class="badges flex row center">
+                    {#if user.contributor}
+                        <div use:tooltip aria-label="contributor" class="flex">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="icon contributor"
+                                viewBox="0 0 512 512"
+                            >
+                                <title>Rocket</title>
+                                <path
+                                    fill="currentColor"
+                                    d="M328.85 156.79a26.69 26.69 0 1018.88 7.81 26.6 26.6 0 00-18.88-7.81z"
+                                />
+                                <path
+                                    fill="currentColor"
+                                    d="M477.44 50.06a.29.29 0 010-.09 20.4 20.4 0 00-15.13-15.3c-29.8-7.27-76.68.48-128.63 21.28-52.36 21-101.42 52-134.58 85.22A320.7 320.7 0 00169.55 175c-22.33-1-42 2.18-58.57 9.41-57.74 25.41-74.23 90.44-78.62 117.14a25 25 0 0027.19 29h.13l64.32-7.02c.08.82.17 1.57.24 2.26a34.36 34.36 0 009.9 20.72l31.39 31.41a34.27 34.27 0 0020.71 9.91l2.15.23-7 64.24v.13A25 25 0 00206 480a25.25 25.25 0 004.15-.34C237 475.34 302 459.05 327.34 401c7.17-16.46 10.34-36.05 9.45-58.34a314.78 314.78 0 0033.95-29.55c33.43-33.26 64.53-81.92 85.31-133.52 20.69-51.36 28.48-98.59 21.39-129.53zM370.38 224.94a58.77 58.77 0 110-83.07 58.3 58.3 0 010 83.07z"
+                                />
+                                <path
+                                    fill="currentColor"
+                                    d="M161.93 386.44a16 16 0 00-11 2.67c-6.39 4.37-12.81 8.69-19.29 12.9-13.11 8.52-28.79-6.44-21-20l12.15-21a16 16 0 00-15.16-24.91A61.25 61.25 0 0072 353.56c-3.66 3.67-14.79 14.81-20.78 57.26A357.94 357.94 0 0048 447.59 16 16 0 0064 464h.4a359.87 359.87 0 0036.8-3.2c42.47-6 53.61-17.14 57.27-20.8a60.49 60.49 0 0017.39-35.74 16 16 0 00-13.93-17.82z"
+                                />
+                            </svg>
+                        </div>
+                    {/if}
+
+                    {#if user.supporter}
+                        <div
+                            use:tooltip
+                            aria-label="supporter for {user.supporter} months"
+                            class="flex"
                         >
-                            <title>Rocket</title>
-                            <path
-                                fill="currentColor"
-                                d="M328.85 156.79a26.69 26.69 0 1018.88 7.81 26.6 26.6 0 00-18.88-7.81z"
-                            />
-                            <path
-                                fill="currentColor"
-                                d="M477.44 50.06a.29.29 0 010-.09 20.4 20.4 0 00-15.13-15.3c-29.8-7.27-76.68.48-128.63 21.28-52.36 21-101.42 52-134.58 85.22A320.7 320.7 0 00169.55 175c-22.33-1-42 2.18-58.57 9.41-57.74 25.41-74.23 90.44-78.62 117.14a25 25 0 0027.19 29h.13l64.32-7.02c.08.82.17 1.57.24 2.26a34.36 34.36 0 009.9 20.72l31.39 31.41a34.27 34.27 0 0020.71 9.91l2.15.23-7 64.24v.13A25 25 0 00206 480a25.25 25.25 0 004.15-.34C237 475.34 302 459.05 327.34 401c7.17-16.46 10.34-36.05 9.45-58.34a314.78 314.78 0 0033.95-29.55c33.43-33.26 64.53-81.92 85.31-133.52 20.69-51.36 28.48-98.59 21.39-129.53zM370.38 224.94a58.77 58.77 0 110-83.07 58.3 58.3 0 010 83.07z"
-                            />
-                            <path
-                                fill="currentColor"
-                                d="M161.93 386.44a16 16 0 00-11 2.67c-6.39 4.37-12.81 8.69-19.29 12.9-13.11 8.52-28.79-6.44-21-20l12.15-21a16 16 0 00-15.16-24.91A61.25 61.25 0 0072 353.56c-3.66 3.67-14.79 14.81-20.78 57.26A357.94 357.94 0 0048 447.59 16 16 0 0064 464h.4a359.87 359.87 0 0036.8-3.2c42.47-6 53.61-17.14 57.27-20.8a60.49 60.49 0 0017.39-35.74 16 16 0 00-13.93-17.82z"
-                            />
-                        </svg>
-                    </div>
-                {/if}
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="icon supporter"
+                                viewBox="0 0 512 512"
+                            >
+                                <title>Heart</title>
+                                <path
+                                    fill="currentColor"
+                                    d="M256 448a32 32 0 01-18-5.57c-78.59-53.35-112.62-89.93-131.39-112.8-40-48.75-59.15-98.8-58.61-153C48.63 114.52 98.46 64 159.08 64c44.08 0 74.61 24.83 92.39 45.51a6 6 0 009.06 0C278.31 88.81 308.84 64 352.92 64c60.62 0 110.45 50.52 111.08 112.64.54 54.21-18.63 104.26-58.61 153-18.77 22.87-52.8 59.45-131.39 112.8a32 32 0 01-18 5.56z"
+                                />
+                            </svg>
+                        </div>
+                    {/if}
 
-                {#if user.supporter}
-                    <div
-                        use:tooltip
-                        aria-label="supporter for {user.supporter} months"
-                        class="flex"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="icon supporter"
-                            viewBox="0 0 512 512"
+                    {#if isCurrentUser}
+                        <a
+                            href="/settings"
+                            use:tooltip
+                            aria-label="profile settings"
+                            class="settings btn"
                         >
-                            <title>Heart</title>
-                            <path
-                                fill="currentColor"
-                                d="M256 448a32 32 0 01-18-5.57c-78.59-53.35-112.62-89.93-131.39-112.8-40-48.75-59.15-98.8-58.61-153C48.63 114.52 98.46 64 159.08 64c44.08 0 74.61 24.83 92.39 45.51a6 6 0 009.06 0C278.31 88.81 308.84 64 352.92 64c60.62 0 110.45 50.52 111.08 112.64.54 54.21-18.63 104.26-58.61 153-18.77 22.87-52.8 59.45-131.39 112.8a32 32 0 01-18 5.56z"
-                            />
-                        </svg>
-                    </div>
-                {/if}
-
-                {#if isLoggedIn}
-                    <a href="/settings" use:tooltip aria-label="profile settings" class="settings btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 512 512">
-                            <title>Settings</title>
-                            <circle fill="currentColor" cx="256" cy="256" r="48" />
-                            <path
-                                fill="currentColor"
-                                d="M470.39 300l-.47-.38-31.56-24.75a16.11 16.11 0 01-6.1-13.33v-11.56a16 16 0 016.11-13.22L469.92 212l.47-.38a26.68 26.68 0 005.9-34.06l-42.71-73.9a1.59 1.59 0 01-.13-.22A26.86 26.86 0 00401 92.14l-.35.13-37.1 14.93a15.94 15.94 0 01-14.47-1.29q-4.92-3.1-10-5.86a15.94 15.94 0 01-8.19-11.82l-5.59-39.59-.12-.72A27.22 27.22 0 00298.76 26h-85.52a26.92 26.92 0 00-26.45 22.39l-.09.56-5.57 39.67a16 16 0 01-8.13 11.82 175.21 175.21 0 00-10 5.82 15.92 15.92 0 01-14.43 1.27l-37.13-15-.35-.14a26.87 26.87 0 00-32.48 11.34l-.13.22-42.77 73.95a26.71 26.71 0 005.9 34.1l.47.38 31.56 24.75a16.11 16.11 0 016.1 13.33v11.56a16 16 0 01-6.11 13.22L42.08 300l-.47.38a26.68 26.68 0 00-5.9 34.06l42.71 73.9a1.59 1.59 0 01.13.22 26.86 26.86 0 0032.45 11.3l.35-.13 37.07-14.93a15.94 15.94 0 0114.47 1.29q4.92 3.11 10 5.86a15.94 15.94 0 018.19 11.82l5.56 39.59.12.72A27.22 27.22 0 00213.24 486h85.52a26.92 26.92 0 0026.45-22.39l.09-.56 5.57-39.67a16 16 0 018.18-11.82c3.42-1.84 6.76-3.79 10-5.82a15.92 15.92 0 0114.43-1.27l37.13 14.95.35.14a26.85 26.85 0 0032.48-11.34 2.53 2.53 0 01.13-.22l42.71-73.89a26.7 26.7 0 00-5.89-34.11zm-134.48-40.24a80 80 0 11-83.66-83.67 80.21 80.21 0 0183.66 83.67z"
-                            />
-                        </svg>
-                    </a>
-                {/if}
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="icon"
+                                viewBox="0 0 512 512"
+                            >
+                                <title>Settings</title>
+                                <circle fill="currentColor" cx="256" cy="256" r="48" />
+                                <path
+                                    fill="currentColor"
+                                    d="M470.39 300l-.47-.38-31.56-24.75a16.11 16.11 0 01-6.1-13.33v-11.56a16 16 0 016.11-13.22L469.92 212l.47-.38a26.68 26.68 0 005.9-34.06l-42.71-73.9a1.59 1.59 0 01-.13-.22A26.86 26.86 0 00401 92.14l-.35.13-37.1 14.93a15.94 15.94 0 01-14.47-1.29q-4.92-3.1-10-5.86a15.94 15.94 0 01-8.19-11.82l-5.59-39.59-.12-.72A27.22 27.22 0 00298.76 26h-85.52a26.92 26.92 0 00-26.45 22.39l-.09.56-5.57 39.67a16 16 0 01-8.13 11.82 175.21 175.21 0 00-10 5.82 15.92 15.92 0 01-14.43 1.27l-37.13-15-.35-.14a26.87 26.87 0 00-32.48 11.34l-.13.22-42.77 73.95a26.71 26.71 0 005.9 34.1l.47.38 31.56 24.75a16.11 16.11 0 016.1 13.33v11.56a16 16 0 01-6.11 13.22L42.08 300l-.47.38a26.68 26.68 0 00-5.9 34.06l42.71 73.9a1.59 1.59 0 01.13.22 26.86 26.86 0 0032.45 11.3l.35-.13 37.07-14.93a15.94 15.94 0 0114.47 1.29q4.92 3.11 10 5.86a15.94 15.94 0 018.19 11.82l5.56 39.59.12.72A27.22 27.22 0 00213.24 486h85.52a26.92 26.92 0 0026.45-22.39l.09-.56 5.57-39.67a16 16 0 018.18-11.82c3.42-1.84 6.76-3.79 10-5.82a15.92 15.92 0 0114.43-1.27l37.13 14.95.35.14a26.85 26.85 0 0032.48-11.34 2.53 2.53 0 01.13-.22l42.71-73.89a26.7 26.7 0 00-5.89-34.11zm-134.48-40.24a80 80 0 11-83.66-83.67 80.21 80.21 0 0183.66 83.67z"
+                                />
+                            </svg>
+                        </a>
+                    {/if}
+                </div>
             </div>
-        </div>
 
-        <p class="joined" use:tooltip aria-label={new Date(user.createdAt).toString()}>
-            joined: {relativeJoined}
-        </p>
-    </div>
-</section>
+            <p class="joined" use:tooltip aria-label={new Date(user.createdAt).toString()}>
+                joined: {relativeJoined}
+            </p>
+        </div>
+    </section>
+
+    <section class="public-pastes">
+        <h3>public pastes</h3>
+
+        <p class="no-public-pastes">{user.username} doesn't have any public pastes yet.</p>
+    </section>
+</div>
 
 <style lang="scss">
+    .layout {
+    }
+
     .user-header {
+        margin-bottom: 0;
+
         .avatar {
             display: inline-block;
             border-radius: $border-radius;
@@ -173,6 +195,38 @@
                 color: $color-bg-3;
                 margin: 0;
                 margin-top: 0.25rem;
+            }
+        }
+    }
+
+    .public-pastes {
+        flex-grow: 1;
+        height: 100%;
+
+        h3 {
+            font-weight: normal;
+            font-size: $fs-medium;
+            margin: 0;
+            margin-bottom: 1rem;
+        }
+
+        .no-public-pastes {
+            text-align: center;
+            margin: 0;
+            font-size: $fs-normal;
+        }
+    }
+
+    @media screen and (min-width: $break-med) {
+        .user-header {
+            min-width: 256px;
+            max-width: 33.33%;
+            margin-right: 2rem;
+
+            .avatar {
+                max-width: 100%;
+                margin-right: 0;
+                margin-bottom: 1rem;
             }
         }
     }
