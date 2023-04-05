bdf030e95dc356176dfa34de6ddaba01677774e9
CodeMyst <code@myst.rs>
2023-04-05 16:46:57 +0200

    fix: not showing tag list for other users

--- client/src/routes/~[user]/+page.svelte
+++ client/src/routes/~[user]/+page.svelte
@@ -113,43 +113,53 @@
             </div>
         </section>
 
-        <details class="tags" open={data.tag !== null}>
-            <summary class="flex row center">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="icon chevron">
-                    <path
-                        fill="currentColor"
-                        d="M6.22 3.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L9.94 8 6.22 4.28a.75.75 0 0 1 0-1.06Z"
-                    />
-                </svg>
-                <span class="summary-title">tags</span>
-
-                {#if data.tag}
-                    <a
-                        href="/~{data.self?.username}"
-                        class="btn btn-icon clear-tag-filter btn-danger"
-                        aria-label="clear the tag filter"
-                        use:tooltip
+        {#if data.tags?.length > 0}
+            <details class="tags" open={data.tag !== null}>
+                <summary class="flex row center">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        class="icon chevron"
                     >
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="icon">
-                            <path
-                                fill="currentColor"
-                                d="M3.72 3.72a.75.75 0 0 1 1.06 0L8 6.94l3.22-3.22a.749.749 0 0 1 1.275.326.749.749 0 0 1-.215.734L9.06 8l3.22 3.22a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L8 9.06l-3.22 3.22a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L6.94 8 3.72 4.78a.75.75 0 0 1 0-1.06Z"
-                            />
-                        </svg>
-                    </a>
-                {/if}
-            </summary>
+                        <path
+                            fill="currentColor"
+                            d="M6.22 3.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L9.94 8 6.22 4.28a.75.75 0 0 1 0-1.06Z"
+                        />
+                    </svg>
+                    <span class="summary-title">tags</span>
 
-            <div class="tags-wrapper">
-                {#each data.tags as tag}
-                    <a
-                        href="/~{data.self?.username}?tag={tag}"
-                        class="btn"
-                        class:active={tag == data.tag}>{tag}</a
-                    >
-                {/each}
-            </div>
-        </details>
+                    {#if data.tag}
+                        <a
+                            href="/~{data.self?.username}"
+                            class="btn btn-icon clear-tag-filter btn-danger"
+                            aria-label="clear the tag filter"
+                            use:tooltip
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 16 16"
+                                class="icon"
+                            >
+                                <path
+                                    fill="currentColor"
+                                    d="M3.72 3.72a.75.75 0 0 1 1.06 0L8 6.94l3.22-3.22a.749.749 0 0 1 1.275.326.749.749 0 0 1-.215.734L9.06 8l3.22 3.22a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L8 9.06l-3.22 3.22a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L6.94 8 3.72 4.78a.75.75 0 0 1 0-1.06Z"
+                                />
+                            </svg>
+                        </a>
+                    {/if}
+                </summary>
+
+                <div class="tags-wrapper">
+                    {#each data.tags as tag}
+                        <a
+                            href="/~{data.self?.username}?tag={tag}"
+                            class="btn"
+                            class:active={tag == data.tag}>{tag}</a
+                        >
+                    {/each}
+                </div>
+            </details>
+        {/if}
     </div>
 
     <div class="pastes">
