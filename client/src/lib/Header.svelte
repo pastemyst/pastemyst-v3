6757cb41014cb9c05ba59170034736326ea632f8
CodeMyst <code@myst.rs>
2024-05-21 23:31:30 +0200

    feat: show an alpha release notice in the header

--- client/src/lib/Header.svelte
+++ client/src/lib/Header.svelte
@@ -3,6 +3,10 @@
     import { tooltip } from "./tooltips";
     import { env } from "$env/dynamic/public";
 
+    const isAlphaRelease = env.PUBLIC_ALPHA_RELEASE === "true";
+
+    const conditionalUseTooltip = isAlphaRelease ? tooltip : () => ({});
+
     const onMenuClick = () => {
         cmdPalTitle.set(null);
         cmdPalOpen.set(true);
@@ -10,9 +14,28 @@
 </script>
 
 <header class="flex row center space-between">
-    <a href="/" class="title btn">
+    <a
+        href="/"
+        class:alpha={isAlphaRelease}
+        class="title btn"
+        aria-label={isAlphaRelease
+            ? "this is an alpha release of pastemyst, use only for testing, things will break and data will get lost"
+            : undefined}
+        use:conditionalUseTooltip
+    >
         <img src="/images/pastemyst.svg" alt="pastemyst logo" />
-        <h1>pastemyst</h1>
+
+        <h1>pastemyst{isAlphaRelease ? "-alpha" : ""}</h1>
+
+        {#if isAlphaRelease}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="icon">
+                <title>Danger Icon</title>
+                <path
+                    fill="currentColor"
+                    d="M6.457 1.047c.659-1.234 2.427-1.234 3.086 0l6.082 11.378A1.75 1.75 0 0 1 14.082 15H1.918a1.75 1.75 0 0 1-1.543-2.575Zm1.763.707a.25.25 0 0 0-.44 0L1.698 13.132a.25.25 0 0 0 .22.368h12.164a.25.25 0 0 0 .22-.368Zm.53 3.996v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.5 0ZM9 11a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"
+                />
+            </svg>
+        {/if}
     </a>
 
     <div class="flex row center">
@@ -78,6 +101,19 @@
 
     .title {
         text-decoration: none;
+
+        &.alpha {
+            border-color: var(--color-danger);
+
+            h1 {
+                color: var(--color-danger);
+            }
+
+            .icon {
+                margin-left: 0.5rem;
+                color: var(--color-danger);
+            }
+        }
 
         h1 {
             font-size: $fs-normal;
