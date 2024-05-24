192889964a09973a6bb06f285a94abf36a79e809
CodeMyst <code@myst.rs>
2024-05-24 15:45:15 +0200

    feat: pin/unpin pastes from profile page

--- client/src/lib/PasteListItem.svelte
+++ client/src/lib/PasteListItem.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import moment from "moment";
     import Dropdown from "./Dropdown.svelte";
-    import { ExpiresIn, deletePaste, type PasteWithLangStats } from "./api/paste";
+    import { ExpiresIn, deletePaste, type PasteWithLangStats, pinPaste } from "./api/paste";
     import { cmdPalOpen, cmdPalTitle, currentUserStore } from "./stores";
     import { tooltip } from "./tooltips";
     import type { LangStat } from "./api/lang";
@@ -38,6 +38,12 @@
 
         cmdPalTitle.set("are you sure you want to delete this paste? this action can't be undone.");
         cmdPalOpen.set(true);
+    };
+
+    const onPin = async () => {
+        dropdown.hide();
+        await pinPaste(pasteWithLangStats.paste.id);
+        invalidateAll();
     };
 </script>
 
@@ -94,6 +100,33 @@
 
                     <div slot="dropdown">
                         <div class="dropdown flex col gap-s">
+                            <button class="dropdown-option flex gap-s" on:click={onPin}>
+                                {#if pasteWithLangStats.paste.pinned}
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="icon">
+                                        <title>Unpin Icon</title>
+                                        <path
+                                            fill="currentColor"
+                                            d="m1.655.595 13.75 13.75q.22.219.22.53 0 .311-.22.53-.219.22-.53.22-.311 0-.53-.22L.595 1.655q-.22-.219-.22-.53 0-.311.22-.53.219-.22.53-.22.311 0 .53.22ZM.72 14.22l4.5-4.5q.219-.22.53-.22.311 0 .53.22.22.219.22.53 0 .311-.22.53l-4.5 4.5q-.219.22-.53.22-.311 0-.53-.22-.22-.219-.22-.53 0-.311.22-.53Z"
+                                        />
+                                        <path
+                                            fill="currentColor"
+                                            d="m5.424 6.146-1.759.419q-.143.034-.183.175-.04.141.064.245l5.469 5.469q.104.104.245.064.141-.04.175-.183l.359-1.509q.072-.302.337-.465.264-.163.567-.091.302.072.465.337.162.264.09.567l-.359 1.509q-.238.999-1.226 1.278-.988.28-1.714-.446L2.485 8.046q-.726-.726-.446-1.714.279-.988 1.278-1.226l1.759-.419q.303-.072.567.091.265.163.337.465.072.302-.091.567-.163.264-.465.336ZM7.47 3.47q.155-.156.247-.355l.751-1.627Q8.851.659 9.75.498q.899-.16 1.544.486l3.722 3.722q.646.645.486 1.544-.161.899-.99 1.282l-1.627.751q-.199.092-.355.247-.219.22-.53.22-.311 0-.53-.22-.22-.219-.22-.53 0-.311.22-.53.344-.345.787-.549l1.627-.751q.118-.055.141-.183.023-.128-.069-.221l-3.722-3.722q-.092-.092-.221-.069-.128.023-.183.141l-.751 1.627q-.204.443-.549.787-.219.22-.53.22-.311 0-.53-.22-.22-.219-.22-.53 0-.311.22-.53Z"
+                                        />
+                                    </svg>
+                                {:else}
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="icon">
+                                        <title>Pin Icon</title>
+                                        <path
+                                            fill="currentColor"
+                                            fill-rule="evenodd"
+                                            d="M4.456.734a1.75 1.75 0 012.826.504l.613 1.327a3.081 3.081 0 002.084 1.707l2.454.584c1.332.317 1.8 1.972.832 2.94L11.06 10l3.72 3.72a.75.75 0 11-1.061 1.06L10 11.06l-2.204 2.205c-.968.968-2.623.5-2.94-.832l-.584-2.454a3.081 3.081 0 00-1.707-2.084l-1.327-.613a1.75 1.75 0 01-.504-2.826L4.456.734zM5.92 1.866a.25.25 0 00-.404-.072L1.794 5.516a.25.25 0 00.072.404l1.328.613A4.582 4.582 0 015.73 9.63l.584 2.454a.25.25 0 00.42.12l5.47-5.47a.25.25 0 00-.12-.42L9.63 5.73a4.581 4.581 0 01-3.098-2.537L5.92 1.866z"
+                                        />
+                                    </svg>
+                                {/if}
+
+                                <p>{pasteWithLangStats.paste.pinned ? "unpin" : "pin"}</p>
+                            </button>
+
                             <button class="dropdown-option delete flex gap-s" on:click={onDelete}>
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
