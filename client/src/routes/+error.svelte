16fe2001b016caa372620ee9c66c9033e56dc977
CodeMyst <code@myst.rs>
2024-09-23 20:57:00 +0200

    feat: custom error page

---
+++ client/src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+</script>
+
+<section class="markdown">
+    <h1>error {$page.status}</h1>
+
+    {#if $page.status === 404}
+        <p>not found</p>
+    {:else}
+        <p>{$page.error?.message}</p>
+    {/if}
+
+    <p>you can go back to the <a href="/">home page</a></p>
+</section>
+
