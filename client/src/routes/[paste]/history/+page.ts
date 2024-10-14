bb6c46d7ea5a3560370b8173badcf660060fae93
CodeMyst <code@myst.rs>
2024-10-14 20:39:29 +0200

    feat: showing paste header on history list page

--- client/src/routes/[paste]/history/+page.ts
+++ client/src/routes/[paste]/history/+page.ts
@@ -1,6 +1,7 @@
-import { getPaste, getPasteHistoryCompact } from "$lib/api/paste";
+import { getPaste, getPasteHistoryCompact, getPasteLangs, getPasteStats, isPasteStarred } from "$lib/api/paste";
 import { error } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
+import { getUserById } from "$lib/api/user";
 
 export const load: PageLoad = async ({ params, fetch }) => {
     const [paste, pasteStatus] = await getPaste(fetch, params.paste);
@@ -10,6 +11,18 @@ export const load: PageLoad = async ({ params, fetch }) => {
         error(pasteStatus);
     }
 
+    const [owner, ownerStatus] =
+        paste.ownerId !== null ? await getUserById(fetch, paste.ownerId) : [null, 0];
+
+    if (paste.ownerId !== null && !owner) {
+        // TODO: error handling
+        error(ownerStatus);
+    }
+
+    const pasteStats = await getPasteStats(fetch, paste.id);
+    const langStats = await getPasteLangs(fetch, paste.id);
+    const isStarred = await isPasteStarred(fetch, paste.id);
+
     const history = await getPasteHistoryCompact(fetch, params.paste);
 
     if (history.length === 0) {
@@ -17,6 +30,6 @@ export const load: PageLoad = async ({ params, fetch }) => {
     }
 
     return {
-        history, paste
+        history, paste, owner, pasteStats, langStats, isStarred
     };
 };
