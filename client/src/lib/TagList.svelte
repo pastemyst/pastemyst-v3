adba956035fbb9c101c1d1decd0ad57263f14f95
CodeMyst <code@myst.rs>
2024-10-14 20:33:34 +0200

    feat: refactor paste page into smaller components

---
+++ client/src/lib/TagList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let tags: string[];
+    export let ownerUsername: string;
+</script>
+
+<div class="tags flex row center">
+    {#each tags as tag}
+        <a href="/~{ownerUsername}?tag={tag}" class="btn">{tag}</a>
+    {/each}
+</div>
+
+<style lang="scss">
+    .tags {
+        font-size: $fs-small;
+        gap: 0.5rem;
+        flex-wrap: wrap;
+        margin-top: 1rem;
+    }
+</style>
