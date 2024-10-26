9752140850c3996cd47003a46ff83676c434fb67
CodeMyst <code@myst.rs>
2024-10-26 22:06:28 +0200

    feat: scroll markdown heading into view

--- client/src/lib/Markdown.svelte
+++ client/src/lib/Markdown.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
     import { marked } from "marked";
     import { markedHeadingAnchorExtension } from "./marked-heading-anchor";
+    import { browser } from "$app/environment";
+    import { onMount } from "svelte";
 
     export let content: string;
 
     marked.use(markedHeadingAnchorExtension());
 
-    let markdownHtml = marked.parse(content, { gfm: true }) as string;
+    $: markdownHtml = marked.parse(content, { gfm: true }) as string;
+
+    onMount(() => {
+        if (browser && window.location.hash) {
+            const id = window.location.hash.slice(1);
+            const element = document.getElementById(id);
+            if (element) {
+                element.scrollIntoView();
+            }
+        }
+    });
 </script>
 
 <div class="markdown">
