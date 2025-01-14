17274f89b68b27d6b9d8fa1e148a0ae71bc96d45
CodeMyst <code@myst.rs>
2025-01-14 15:26:20 +0100

    feat: storing scopes in UserContext

--- api/Middleware/UserContextMiddleware.cs
+++ api/Middleware/UserContextMiddleware.cs
@@ -6,10 +6,10 @@ public class UserContextMiddleware(RequestDelegate next)
 {
     public async Task InvokeAsync(HttpContext context, AuthService authService)
     {
-        var self = await authService.GetSelfAsync(context);
+        var (self, scopes) = await authService.GetSelfWithScopesAsync(context);
 
         var userContext = context.RequestServices.GetRequiredService<UserContext>();
-        userContext.LoginUser(self);
+        userContext.LoginUser(self, scopes);
 
         context.Features.Set(userContext);
 
--- api/Services/UserContext.cs
+++ api/Services/UserContext.cs
@@ -1,16 +1,23 @@
 using pastemyst.Models;
+using pastemyst.Models.Auth;
 
 namespace pastemyst.Services;
 
 public class UserContext
 {
-    public User Self { get; private set;  }
+    public User Self { get; private set; }
+
+    public Scope[] Scopes { get; private set; }
 
     public bool IsLoggedIn() => Self is not null;
 
     public bool UserIsSelf(User user) => Self is not null && Self.Id == user.Id;
 
-    public void LoginUser(User user) => Self = user;
+    public void LoginUser(User user, Scope[] scopes)
+    {
+        Self = user;
+        Scopes = scopes;
+    }
 
     public void LogoutUser() => Self = null;
 }
