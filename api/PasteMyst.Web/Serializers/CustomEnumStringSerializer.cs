3b4ebcd9af3b42858004a05cbb92510e0486a3ea
CodeMyst <code@myst.rs>
2025-01-30 14:48:23 +0100

    feat: upgrade mongo driver to 3

--- api/PasteMyst.Web/Serializers/CustomEnumStringSerializer.cs
+++ api/PasteMyst.Web/Serializers/CustomEnumStringSerializer.cs
@@ -1,33 +1,48 @@
 using System.Reflection;
 using System.Runtime.Serialization;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
 
 namespace PasteMyst.Web.Serializers;
 
-public class CustomEnumStringSerializer<TEnum> : EnumSerializer<TEnum>
-    where TEnum : struct, System.Enum, IComparable, IFormattable, IConvertible
+public class CustomEnumStringSerializer<TEnum> : IBsonSerializer<TEnum> where TEnum : struct, Enum
 {
-    public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, TEnum value)
-    {
-        var enumValue = typeof(TEnum)
-            .GetTypeInfo()
-            .GetFields()
-            .SingleOrDefault(e => e.Name == value.ToString())!
-            .GetCustomAttribute<EnumMemberAttribute>(false)
-            ?.Value;
+    public Type ValueType => typeof(TEnum);
 
-        context.Writer.WriteString(enumValue);
+    public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, TEnum value)
+    {
+        var enumMemberValue = value.GetType()
+            .GetField(value.ToString())
+            ?.GetCustomAttribute<EnumMemberAttribute>()
+            ?.Value ?? value.ToString();
+
+        context.Writer.WriteString(enumMemberValue);
     }
 
-    public override TEnum Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+    public TEnum Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
     {
-        var enumValue = context.Reader.ReadString();
+        var enumString = context.Reader.ReadString();
 
-        return (TEnum) typeof(TEnum)
-            .GetTypeInfo()
-            .GetFields()
-            .SingleOrDefault(e => e?.GetCustomAttribute<EnumMemberAttribute>()?.Value == enumValue)
-            ?.GetValue(null)!;
+        foreach (var field in typeof(TEnum).GetFields())
+        {
+            var attribute = field.GetCustomAttribute<EnumMemberAttribute>();
+            if (attribute is not null && attribute.Value == enumString)
+            {
+                return (TEnum)field.GetValue(null);
+            }
+        }
+
+        if (Enum.TryParse(enumString, out TEnum result))
+        {
+            return result;
+        }
+
+        throw new BsonSerializationException($"Cannot deserialize '{enumString}' to {typeof(TEnum).Name}");
     }
+
+    public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, object value)
+        => Serialize(context, args, (TEnum)value);
+
+    object IBsonSerializer.Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        => Deserialize(context, args);
 }
