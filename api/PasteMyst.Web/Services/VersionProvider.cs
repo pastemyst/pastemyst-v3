e1499e427a0e4b293760321917d90f5baa009d97
CodeMyst <code@myst.rs>
2025-02-06 20:03:17 +0100

    fix: fixed semver version ordering

--- api/PasteMyst.Web/Services/VersionProvider.cs
+++ api/PasteMyst.Web/Services/VersionProvider.cs
@@ -5,13 +5,13 @@ namespace PasteMyst.Web.Services;
 
 public sealed class VersionProvider
 {
-    private string _version = null!;
+    private string version = null!;
 
     public string GetVersion()
     {
         // If we've already calculated the version, return it
-        if (_version is not null)
-            return _version;
+        if (version is not null)
+            return version;
 
         // Otherwise, get the version information from the .git folder in the repository
         var repoPath = "../../";
@@ -22,12 +22,11 @@ public sealed class VersionProvider
 
         using var repo = new Repository(repoPath);
 
-        _version = repo.Tags
-            .Select(t => SemVersion.Parse(t.FriendlyName, SemVersionStyles.Strict))
-            .OrderDescending()
-            .First()
+        version = repo.Tags
+            .Select(t => SemVersion.Parse(t.FriendlyName))
+            .Max(SemVersion.PrecedenceComparer)
             .ToString();
 
-        return _version;
+        return version;
     }
 }
