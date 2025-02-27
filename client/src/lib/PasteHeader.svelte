b540714b07cd77bb48c3a332974f9223c2472581
CodeMyst <code@myst.rs>
2025-02-27 21:49:20 +0100

    feat: copy embed script button

--- client/src/lib/PasteHeader.svelte
+++ client/src/lib/PasteHeader.svelte
@@ -36,6 +36,7 @@
     }: Props = $props();
 
     let linkCopied = $state(false);
+    let embedCopied = $state(false);
 
     const onPinClick = async () => {
         const ok = await pinPaste(paste.id);
@@ -72,6 +73,20 @@
 
         setTimeout(() => {
             linkCopied = false;
+        }, 1000);
+    };
+
+    const onCopyEmbed = async () => {
+        const host = location.protocol + "//" + location.host;
+        // eslint-disable-next-line no-useless-escape
+        const embedScript = `<iframe src='${host}/${paste.id}/embed' scrolling='no' style='border:none;'></iframe><script src='${host}/static/scripts/iframeResizer.js'><\/script><script>iFrameResize();<\/script>`;
+
+        await navigator.clipboard.writeText(embedScript);
+
+        embedCopied = true;
+
+        setTimeout(() => {
+            embedCopied = false;
         }, 1000);
     };
 
@@ -276,6 +291,24 @@
                 />
             </svg>
         </button>
+
+        {#if !paste.private}
+            <button
+                aria-label="copy embed script"
+                use:tooltip={{
+                    content: embedCopied ? "copied" : "copy embed script",
+                    hideOnClick: false
+                }}
+                onclick={onCopyEmbed}
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="icon">
+                    <path
+                        fill="currentColor"
+                        d="M0 3.75C0 2.784.784 2 1.75 2h12.5c.966 0 1.75.784 1.75 1.75v8.5A1.75 1.75 0 0 1 14.25 14H1.75A1.75 1.75 0 0 1 0 12.25Zm1.75-.25a.25.25 0 0 0-.25.25v8.5c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25v-8.5a.25.25 0 0 0-.25-.25ZM3.5 6.25a.75.75 0 0 1 .75-.75h7a.75.75 0 0 1 0 1.5h-7a.75.75 0 0 1-.75-.75Zm.75 2.25h4a.75.75 0 0 1 0 1.5h-4a.75.75 0 0 1 0-1.5Z"
+                    />
+                </svg>
+            </button>
+        {/if}
 
         <a
             href="{env.PUBLIC_API_BASE}/pastes/{paste.id}.zip"
