c90a9bfae2f98a2fd7783efdd2b5e514a90b4e36
CodeMyst <code@myst.rs>
2025-03-10 20:50:16 +0100

    feat: v2/time api

---
+++ api/PasteMyst.Web/Models/V2/ExpiresInToUnixTimeResultV2.cs
@@ -0,0 +1,6 @@
+namespace PasteMyst.Web.Models.V2;
+
+public class ExpiresInToUnixTimeResultV2
+{
+    public long Result { get; set; }
+}
--- api/PasteMyst.Web/Utils/ExpiresInUtils.cs
+++ api/PasteMyst.Web/Utils/ExpiresInUtils.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using System.Runtime.Serialization;
 using PasteMyst.Web.Models;
 
 namespace PasteMyst.Web.Utils;
@@ -19,5 +21,24 @@ public static class ExpiresInUtils
             ExpiresIn.OneYear => start.AddYears(1),
             _ => throw new ArgumentOutOfRangeException(nameof(expiresIn), expiresIn, null)
         };
+    }
+
+    public static ExpiresIn? Parse(string expiresIn)
+    {
+        foreach (var field in typeof(ExpiresIn).GetFields())
+        {
+            var attribute = field.GetCustomAttribute<EnumMemberAttribute>();
+            if (attribute is not null && attribute.Value == expiresIn)
+            {
+                return (ExpiresIn) field.GetValue(null);
+            }
+        }
+
+        if (!Enum.TryParse(expiresIn, out ExpiresIn expiresInEnum))
+        {
+            return null;
+        }
+
+        return expiresInEnum;
     }
 }
