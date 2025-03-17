327970b92fbadb7ca2f1380c8679a1fc0de3a40b
CodeMyst <code@myst.rs>
2025-03-17 13:39:40 +0100

    feat: allow empty pasty content

--- api/PasteMyst.Web/Models/PastyCreateInfo.cs
+++ api/PasteMyst.Web/Models/PastyCreateInfo.cs
@@ -7,8 +7,7 @@ public class PastyCreateInfo
     [MaxLength(50)]
     public string Title { get; set; } = "";
 
-    [Required]
-    public string Content { get; set; }
+    public string Content { get; set; } = "";
 
     public string Language { get; set; }
 }
