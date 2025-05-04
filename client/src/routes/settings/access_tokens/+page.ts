8906cc18460a3ee62c8d20dc849c8a2b64a603c0
CodeMyst <code@myst.rs>
2025-05-04 20:55:47 +0200

    fix: forbid settings pages when logged out

--- client/src/routes/settings/access_tokens/+page.ts
+++ client/src/routes/settings/access_tokens/+page.ts
@@ -1,7 +1,12 @@
 import { getAccessTokens } from "$lib/api/auth";
+import { error } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
-export const load: PageLoad = async ({ fetch }) => {
+export const load: PageLoad = async ({ fetch, parent }) => {
+    const { self } = await parent();
+
+    if (!self) throw error(401);
+
     const accessTokens = await getAccessTokens(fetch);
 
     return { accessTokens };
--- client/src/routes/settings/admin/+page.ts
+++ client/src/routes/settings/admin/+page.ts
@@ -1,8 +1,13 @@
 import { getAllAnnouncements } from "$lib/api/announcement";
 import { marked } from "marked";
 import type { PageLoad } from "./$types";
+import { error } from "@sveltejs/kit";
 
-export const load: PageLoad = async ({ fetch }) => {
+export const load: PageLoad = async ({ fetch, parent }) => {
+    const { self } = await parent();
+
+    if (!self) throw error(401);
+
     const announcements = await getAllAnnouncements(fetch);
 
     const renderedMarkdown: string[] = [];
