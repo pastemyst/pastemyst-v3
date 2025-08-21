e47fc0794e38cb663e11bcbff0c853d49662cd2d
CodeMyst <code@myst.rs>
2025-08-21 22:38:11 +0200

    fix: eslint

--- client/src/routes/api-docs/[...slug]/+server.ts
+++ client/src/routes/api-docs/[...slug]/+server.ts
@@ -2,5 +2,5 @@ import { env } from "$env/dynamic/public";
 import { redirect, type RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async () => {
-    redirect(301, env.PUBLIC_DOCS_URL);
+    redirect(301, env.PUBLIC_DOCS_URL!);
 };
