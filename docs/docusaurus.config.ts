638d07abdf9ef942aaeb32473369225e39481de9
CodeMyst <code@myst.rs>
2025-08-25 13:56:04 +0200

    feat: docs now follow the system theme

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -68,6 +68,9 @@ const config: Config = {
   ],
 
   themeConfig: {
+    colorMode: {
+      respectPrefersColorScheme: true
+    },
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
