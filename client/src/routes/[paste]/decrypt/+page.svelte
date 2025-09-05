7004f14382c73d582f7522db39a19b8b9b431546
CodeMyst <code@myst.rs>
2025-09-05 13:30:02 +0200

    fix: added meta tags to encrypted pastes

--- client/src/routes/[paste]/decrypt/+page.svelte
+++ client/src/routes/[paste]/decrypt/+page.svelte
@@ -4,6 +4,12 @@
     let { form }: { form: ActionData } = $props();
 </script>
 
+<svelte:head>
+    <title>pastemyst | encrypted paste</title>
+    <meta property="og:title" content="pastemyst | encrypted paste" />
+    <meta property="twitter:title" content="pastemyst | encrypted paste" />
+</svelte:head>
+
 <section class="block">
     <h2>encrypted paste</h2>
     <p>this paste is encrypted, please enter the encryption password:</p>
@@ -21,6 +27,3 @@
         <input class="btn btn-main" type="submit" value="decrypt" />
     </form>
 </section>
-
-<style lang="scss">
-</style>
